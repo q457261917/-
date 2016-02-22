794ae3e4b138c6cbe1cc4f6aa74525fdfaa6bcc9
q457261917 <xingxing@xingxingdemacbook-pro.local>
2016-02-22 16:39:52 +0800

    初始化项目

---
+++ JasonWeibo/JasonWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JasonWeibo
+//
+//  Created by 星星 on 16/2/22.
+//  Copyright © 2016年 星星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JasonWeibo/JasonWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JasonWeibo
+//
+//  Created by 星星 on 16/2/22.
+//  Copyright © 2016年 星星. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
